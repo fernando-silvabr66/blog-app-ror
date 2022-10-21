af056c72d9efea645931b8b39d85a92ab9b94cc4
Fernando Silva <fernando.silvabr66@gmail.com>
2022-10-21 14:10:23 -0300

    Add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,16 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_initialize do |post|
+    post.likes_counter = 0
+    post.comments_counter = 0
+  end
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
