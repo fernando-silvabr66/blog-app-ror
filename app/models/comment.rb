16ea9f9cab8538da9b90def109461ec82b2c26bd
Fernando Silva <fernando.silvabr66@gmail.com>
2022-10-27 14:15:53 -0300

    Update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
   after_save :update_comments_counter
 
   def update_comments_counter
-    post.increment!(:comments_counter)
+    post.update(comments_counter: post.comments.count)
   end
 
   private :update_comments_counter
