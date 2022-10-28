be3479103b692b45687cba593ec937107d9e7623
Fernando Silva <fernando.silvabr66@gmail.com>
2022-10-28 13:45:23 -0300

    Review spec helpers for user and comments

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Comments', type: :request do
   describe 'GET /' do
     before(:each) do
-      @user = User.create(name: 'David')
+      @user = User.create(name: 'John Doe')
       @id = @user.id
       post = Post.create(title: 'My first post', author: @user, text: 'Text of my first post')
       get "/users/#{@id}/posts/#{post.id}"
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'includes the correct placeholder text in its body' do
-      expect(response.body).to include('<h1>User#Name placeholder</h1>')
+      expect(response.body).to include('<h1>User John Doe</h1>')
     end
   end
 end
