2a7b001bd9afb8d41627ed5f460593fdf1d7889a
Fernando Silva <fernando.silvabr66@gmail.com>
2022-10-28 11:52:45 -0300

    Update like model features1

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,23 @@
-class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
-  belongs_to :post
+class LikesController < ApplicationController
+  def create
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @like = Like.new(like_params)
+    @like.author = @user
+    @like.post = @post
 
-  after_save :update_likes_counter
-
-  def update_likes_counter
-    post.update(likes_counter: post.likes.count)
+    if @like.save
+      flash[:success] = 'Like saved successfully'
+      redirect_to user_post_path(@post.author, @post)
+    else
+      flash.now[:error] = 'Error: Like could not be saved'
+      render :new, status: 422
+    end
   end
 
-  private :update_likes_counter
+  private
+
+  def like_params
+    params.permit(:author, :post)
+  end
 end
