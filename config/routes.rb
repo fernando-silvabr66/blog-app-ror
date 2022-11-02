22f816300ef98204742dba6cabbbcbe13220fd35
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-02 18:17:10 -0300

    Add and configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'comments/create'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
