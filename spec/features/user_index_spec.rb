53464a91247a343d6f6a5625c1ddad9592261e55
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-02 13:05:17 -0300

    Fix user_index_spec tests

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe User, type: :system do
     before(:each) do
       @first_photo = 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
       @second_photo = 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
-      @first_user = User.create(name: 'Fernando', photo: @new_photo, bio: 'Teacher from Brazil.')
-      @second_user = User.create(name: 'Amanda', photo: @new_photo, bio: 'Student from USA.')
+      @first_user = User.create(name: 'Fernando', photo: @first_photo, bio: 'Teacher from Brazil.')
+      @second_user = User.create(name: 'Amanda', photo: @second_photo, bio: 'Student from USA.')
       @third_user = User.create(name: 'Donald', photo: @first_photo, bio: 'Musician from England.')
-      @fourth_user = User.create(name: 'Monica', photo: @first_photo, bio: 'Dancer from Belgium.')
+      @fourth_user = User.create(name: 'Monica', photo: @second_photo, bio: 'Dancer from Belgium.')
       @first_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my first post')
       @second_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my second post')
       @third_post = Post.create(author: @first_user, title: 'Hello', text: 'This is my third post')
