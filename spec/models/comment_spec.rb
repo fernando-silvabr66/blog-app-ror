ad709610af5a1a7ec7d50baf448cab1c720b522c
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-02 11:03:39 -0300

    Fix comments spec test for n+1 issue

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Comment, type: :model do
   # Create Post and users to add new comments
   new_post = Post.create(title: 'My first post', author: User.create(name: 'John Doe'))
   other_user = User.create(name: 'Jane Smith')
-  subject { Comment.create(text: 'Hi comment', author: other_user, post: new_post) }
+  @comment = Comment.create(text: 'Hi comment', author: other_user, post: new_post)
 
   it 'UpdatesCommentsCounter increments post comment_counter' do
-    expect(subject.post.comments_counter).to be 1
-    Comment.create(text: 'Hi comment', author: subject.author, post: subject.post)
-    expect(subject.post.comments_counter).to be 2
+    expect(@comment.post.comments_counter).to be 1
+    Comment.create(text: 'Hi comment', author: @comment.author, post: @comment.post)
+    expect(@comment.post.comments_counter).to be 2
   end
 end
