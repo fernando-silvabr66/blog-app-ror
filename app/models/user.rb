be78f474893650fe41304daec57f9c6a319bfb34
Fernando Silva <fernando.silvabr66@gmail.com>
2022-11-04 15:47:01 -0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User < ApplicationRecord
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable, :confirmable
-  
-    has_many :posts, foreign_key: 'author_id'
-    has_many :likes, foreign_key: 'author_id'
-    has_many :comments, foreign_key: 'author_id'
-  
-    validates :name, presence: true
-    validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, :on => create
-   
-    def last_three_posts
-      Post.includes([:comments]).where(author: self).order(created_at: :desc).first(3)
-    end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, on: create
+
+  def last_three_posts
+    Post.includes([:comments]).where(author: self).order(created_at: :desc).first(3)
   end
-  
+end
